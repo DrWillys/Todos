d676c59b369bd96cef760d3aa24b4eba562a57d7
Erik S <eriksetterquist@gmail.com>
2022-04-20 07:37:48 +0200

    Add task gui

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({ title, onAdd, showAdd}) => {
+    return (
+        <header className='header'>
+            <h1>TODOS {title}</h1>
+            <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: 'Default'
+}
+
+Header.propTypes = {
+    title: PropTypes.string
+}
+
+export default Header 
